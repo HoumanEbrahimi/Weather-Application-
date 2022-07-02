334dbd0b1239c9465041e36009c0ed09d67f4645
Houman Ebrahimi <90071124+HoumanEbrahimi@users.noreply.github.com>
2022-07-02 17:05:09 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Windows.Input;
 using System.Collections.Generic;
 using Newtonsoft.Json;
@@ -7,6 +7,8 @@ using Weather;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System;
+using System.Windows.Media.Imaging;
+using weatherForecast;
 
 namespace Weather
 
@@ -46,45 +48,159 @@ namespace Weather
 
         string apiKey = "2ea3790bda9676de193a19d568e3c12e";
 
+        string unit="metric";
 
-        void weatherInfo()
+        private void Celsius(object sender, RoutedEventArgs e)
         {
-            AllocConsole();
+            unit = "metric";
+
+        }
+
+        private void Fahrenheit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void weatherInfo(object sender,RoutedEventArgs e)
+        {
 
             using (WebClient client = new WebClient())
             {
-                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", txtSearch.Text, apiKey);
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}", txtSearch.Text,unit, apiKey);
                 var json=client.DownloadString(url);
                 weatherInfo.root Info = JsonConvert.DeserializeObject<weatherInfo.root>(json);
 
                 WindSpeed.Text = Info.wind.speed.ToString();
 
-                int result = 0;
-                Console.WriteLine(int.Parse(Info.main.temp.ToString()));
+                Texttemp.Text = Info.main.temp.ToString() + "°c";
+                CountryName.Text=txtSearch.Text+","+Info.sys.country.ToString();
+                WeatherCondition.Text = "Feels like :"+Info.main.feels_like.ToString()+ " °c";
+                HumidityContext.Text = Info.main.humidity.ToString();
 
-                if (int.TryParse(Info.main.temp.ToString(), out result))
+                System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+                System.DateTime dtDateTime2 = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+
+                dtDateTime = dtDateTime.AddSeconds(int.Parse(Info.sys.sunrise.ToString())).ToLocalTime();
+
+                dtDateTime2 = dtDateTime2.AddSeconds(int.Parse(Info.sys.sunset.ToString())).ToLocalTime();
+
+                SunriseContext.Text = dtDateTime.ToString();
+
+                SunsetContext.Text = dtDateTime2.ToString();
+
+                if (int.Parse(Info.main.humidity.ToString())>30 && int.Parse(Info.main.humidity.ToString()) < 50)
                 {
+                    humidContext.Text = "normal";
+                    howHumid.Source = new BitmapImage(new Uri(@"/Images/happy.png", UriKind.Relative));
+                }
 
-                    int val = (int.Parse(Info.main.temp.ToString()) - 32) * 9 / 5;
-
-                    Texttemp.Text = val.ToString() + "°c";
+                else if (int.Parse(Info.main.humidity.ToString()) > 50)
+                {
+                    humidContext.Text = "High humidity";
+                    howHumid.Source = new BitmapImage(new Uri(@"/Images/dislike.png", UriKind.Relative));
 
                 }
-                CountryName.Text=txtSearch.Text+","+Info.sys.country.ToString();
 
-                
+                else if (int.Parse(Info.main.humidity.ToString()) < 30)
+                {
+                    humidContext.Text = "Not humid";
+                    howHumid.Source = new BitmapImage(new Uri(@"/Images/like.png", UriKind.Relative));
+
+                }
+
+
+                if (Info.weather[0].description.ToString() == "clear sky") 
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/sun.png", UriKind.Relative));
+
+                } 
+
+                else if (Info.weather[0].description.ToString() == "broken clouds" || Info.weather[0].description.ToString() == "overcast clouds")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/cloud.png", UriKind.Relative));
+                }
+
+                else if(Info.weather[0].description.ToString() =="shower rain")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/rain.png", UriKind.Relative));
+
+                }
+
+                else if(Info.weather[0].description.ToString() == "rain")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/rain_cloud.png", UriKind.Relative));
+
+                }
+
+
+                else if (Info.weather[0].description.ToString() == "snow")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/snow.png", UriKind.Relative));
+
+                }
+
+                else if (Info.weather[0].description.ToString() == "thunderstorm")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/storm.png", UriKind.Relative));
+
+                }
+
+                else if(Info.weather[0].description.ToString() == "mist")
+                {
+                    imageCondition.Source = new BitmapImage(new Uri(@"/Images/water.png", UriKind.Relative));
+
+                }
+                if (int.Parse(Info.wind.deg.ToString()) > 0 && int.Parse(Info.wind.deg.ToString()) < 90)
+                {
+                    windDirection.Text = "NE";
+                }
+
+                else if (int.Parse(Info.wind.deg.ToString()) == 0 || int.Parse(Info.wind.deg.ToString()) == 360) { windDirection.Text = "E"; }
+
+                else if(int.Parse(Info.wind.deg.ToString()) == 90) { windDirection.Text = "N"; }
+                else if (int.Parse(Info.wind.deg.ToString()) > 90 && int.Parse(Info.wind.deg.ToString()) < 135)
+                {
+                    windDirection.Text = "NW";
+                }
+                else if(int.Parse(Info.wind.deg.ToString())>135 && int.Parse(Info.wind.deg.ToString())<180){ windDirection.Text = "WN"; }
+                else if(int.Parse(Info.wind.deg.ToString()) == 180) { windDirection.Text = "W"; }
+                else if (int.Parse(Info.wind.deg.ToString())>180 && int.Parse(Info.wind.deg.ToString()) < 225) { windDirection.Text = "SW"; }
+                else if(int.Parse(Info.wind.deg.ToString())>225 && int.Parse(Info.wind.deg.ToString()) <270) { windDirection.Text = "WS"; }
+                else if (int.Parse(Info.wind.deg.ToString()) == 270) { windDirection.Text = "S"; }
+                else if(int.Parse(Info.wind.deg.ToString()) >270 && int.Parse(Info.wind.deg.ToString())<360) { windDirection.Text = "SE"; }
 
             }
 
+        }
+
+        void getForecast(object sender, RoutedEventArgs e)
+        {
+          
+           // string url = "";
+            using(WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units={1}&appid={2}", txtSearch.Text, unit, apiKey);
+
+                var json =web.DownloadString(url);
+                weatherForecast.forecast info2 = JsonConvert.DeserializeObject<weatherForecast.forecast>(json);
+
+                tempnext.Text = info2.list[1].main.temp.ToString();
 
 
-
+            }
         }
         private void click(object sender, RoutedEventArgs e)
         {
 
-            weatherInfo();
 
+            weatherInfo(sender,e);
+            getForecast(sender,e);
         }
+
+
+
+
     }
 }
